62af80bdd7fd7c89599705df43bf69c28d38a3e5
AWA <awa?>
2013-03-24 23:17:44 +0100

    Added presents in DB

---
+++ SQL Files/update3.sql
@@ -0,0 +1,17 @@
+INSERT INTO items_base VALUES ('161', '187', 'Gift', 'present_gen', 's', '1', '1', '1.00', '1', '0', '0', '1', '0', '1', '1', '1', 'default', '1', '0');
+INSERT INTO items_base VALUES ('162', '188', 'Gift', 'present_gen1', 's', '1', '1', '1.00', '1', '0', '0', '1', '0', '1', '1', '1', 'default', '1', '0');
+INSERT INTO items_base VALUES ('163', '189', 'Gift', 'present_gen2', 's', '1', '1', '1.00', '1', '0', '0', '1', '0', '1', '1', '1', 'default', '1', '0');
+INSERT INTO items_base VALUES ('164', '190', 'Gift', 'present_gen3', 's', '1', '1', '1.00', '1', '0', '0', '1', '0', '1', '1', '1', 'default', '1', '0');
+INSERT INTO items_base VALUES ('165', '191', 'Gift', 'present_gen4', 's', '1', '1', '1.00', '1', '0', '0', '1', '0', '1', '1', '1', 'default', '1', '0');
+INSERT INTO items_base VALUES ('166', '192', 'Gift', 'present_gen5', 's', '1', '1', '1.00', '1', '0', '0', '1', '0', '1', '1', '1', 'default', '1', '0');
+INSERT INTO items_base VALUES ('167', '193', 'Gift', 'present_gen6', 's', '1', '1', '1.00', '1', '0', '0', '1', '0', '1', '1', '1', 'default', '1', '0');
+INSERT INTO items_base VALUES ('2794', '3372', 'present_wrap*1', 'Caja Regalo 1', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
+INSERT INTO items_base VALUES ('2795', '3373', 'present_wrap*10', 'Caja Regalo 10', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
+INSERT INTO items_base VALUES ('2796', '3374', 'present_wrap*2', 'Caja Regalo 2', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
+INSERT INTO items_base VALUES ('2797', '3375', 'present_wrap*3', 'Caja Regalo 3', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
+INSERT INTO items_base VALUES ('2798', '3376', 'present_wrap*4', 'Caja Regalo 4', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
+INSERT INTO items_base VALUES ('2799', '3377', 'present_wrap*5', 'Caja Regalo 5', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
+INSERT INTO items_base VALUES ('2800', '3378', 'present_wrap*6', 'Caja Regalo 6', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
+INSERT INTO items_base VALUES ('2801', '3379', 'present_wrap*7', 'Caja Regalo 7', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
+INSERT INTO items_base VALUES ('2802', '3380', 'present_wrap*8', 'Caja Regalo 8', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
+INSERT INTO items_base VALUES ('2803', '3381', 'present_wrap*9', 'Caja Regalo 9', 's', '0', '1', '1.00', '1', '0', '0', '0', '0', '0', '0', '1', 'gift', '0', '');
